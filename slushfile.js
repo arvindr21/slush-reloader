2c96ce02c3d7c64ff7a06b0b19d47fbb63d9dcd5
arvindr21 <arvind.ravulavaru@gmail.com>
2014-08-03 19:11:33 +0530

    initial commit

---
+++ slushfile.js
@@ -0,0 +1,64 @@
+/*
+ * slush-reloader
+ * https://github.com/arvindr21/slush-reloader
+ *
+ * Copyright (c) 2014, Arvind Ravulavaru
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var gulp = require('gulp'),
+    install = require('gulp-install'),
+    conflict = require('gulp-conflict'),
+    template = require('gulp-template'),
+    rename = require('gulp-rename'),
+    _ = require('underscore.string'),
+    inquirer = require('inquirer');
+
+function format(string) {
+    var username = string.toLowerCase();
+    return username.replace(/\s/g, '');
+}
+
+var defaults = (function() {
+    var homeDir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
+        workingDirName = process.cwd().split('/').pop().split('\\').pop(),
+        osUserName = homeDir && homeDir.split('/').pop() || 'root',
+        configFile = homeDir + '/.gitconfig',
+        user = {};
+    if (require('fs').existsSync(configFile)) {
+        user = require('iniparser').parseSync(configFile).user;
+    }
+    return {
+        appName: workingDirName,
+        userName: format(user.name) || osUserName,
+        authorEmail: user.email || ''
+    };
+})();
+
+gulp.task('default', function(done) {
+    var prompts = [{
+        name: 'appName',
+        message: 'What is the name of your project?',
+        default: defaults.appName
+    }];
+    //Ask
+    inquirer.prompt(prompts,
+        function(answers) {
+            answers.appNameSlug = _.slugify(answers.appName);
+            gulp.src(__dirname + '/templates/**')
+                .pipe(template(answers))
+                .pipe(rename(function(file) {
+                    if (file.basename[0] === '_') {
+                        file.basename = '.' + file.basename.slice(1);
+                    }
+                }))
+                .pipe(conflict('./'))
+                .pipe(gulp.dest('./'))
+                .pipe(install())
+                .on('end', function() {
+                    done();
+                });
+        });
+});
